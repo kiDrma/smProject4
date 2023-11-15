4bae1487b6ecbde12e5c868637c92f23f5f59bb2
DanQZ <danqzbusiness@gmail.com>
2023-11-15 17:23:15 -0500

    custom pizza build

--- src/main/java/com/example/smproject4/BuildYourOwnController.java
+++ src/main/java/com/example/smproject4/BuildYourOwnController.java
@@ -2,8 +2,83 @@ package com.example.smproject4;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class BuildYourOwnController {
+    @FXML
+    private VBox toppingsVBox;
+    @FXML
+    private ArrayList<Topping> selectedToppings(){
+
+        // creates Topping array of all toppings selected
+        ArrayList<Topping> toppingsAdded = new ArrayList<Topping>();
+        int addedToppingsCount = 0;
+        for (javafx.scene.Node node : toppingsVBox.getChildren()) {
+            if (node instanceof CheckBox) {
+                CheckBox checkBox = (CheckBox) node;
+                //System.out.println("Checkbox: " + checkBox.getText() + ", Selected: " + checkBox.isSelected());
+                if(checkBox.isSelected()){
+                    Topping newTopping = Topping.getToppingFromName(checkBox.getText());
+                    toppingsAdded.add(newTopping);
+                    addedToppingsCount++;
+                }
+            }
+        }
+
+
+        //debug
+        /*String debugString = "Selected toppings: \n";
+        for (int i = 0; i < addedToppingsCount; i++) {
+            debugString += toppingsAdded.get(i).toString() + "\n";
+        }
+        System.out.println(debugString);
+        */
+
+
+        return toppingsAdded;
+    }
+
+    @FXML
+    private Custom buildPizza(){
+        /*
+        public Custom(Topping[] toppings, Sauce sauce, Size size,
+                      boolean extraCheese, boolean extraSauce){
+            initializeToppings(toppings);
+            this.sauce = sauce;
+            this.size = size;
+            this.extraCheese = extraCheese;
+            this.extraSauce = extraSauce;
+        }
+        */
+
+        ArrayList<Topping> customToppings = selectedToppings();
+        Sauce customSauce = null;
+        if(alfredoButton.isSelected()){customSauce = Sauce.ALFREDO;}
+        if(tomatoButton.isSelected()){customSauce = Sauce.TOMATO;}
+
+        Size customSize = null;
+        if(largeButton.isSelected()){customSize = Size.LARGE;}
+        if(mediumButton.isSelected()){customSize = Size.MEDIUM;}
+        if(smallButton.isSelected()){customSize = Size.SMALL;}
+
+        boolean extraCheese = extraCheeseBox.isSelected();
+        boolean extraSauce = extraSauceBox.isSelected();
+
+        if(customSauce == null || customSize == null){
+            System.out.println("DID NOT SELECT SAUCE AND/OR SIZE");
+            return null;
+        }
+
+        Custom output = new Custom(customToppings, customSauce, customSize, extraCheese, extraSauce);
+        System.out.println(output.toString());
+        return output;
+    }
+
+
+
     @FXML
     private RadioButton smallButton;
     @FXML
@@ -15,9 +90,9 @@ public class BuildYourOwnController {
     @FXML
     private RadioButton alfredoButton;
     @FXML
-    private CheckBox extraCheese;
+    private CheckBox extraCheeseBox;
     @FXML
-    private CheckBox extraSauce;
+    private CheckBox extraSauceBox;
     @FXML
     private ListView toppingsList;
     @FXML
--- src/main/java/com/example/smproject4/Topping.java
+++ src/main/java/com/example/smproject4/Topping.java
@@ -11,5 +11,27 @@ public enum Topping {
     BEEF,
     SHRIMP,
     SQUID,
-    CRAB_MEATS; // add two more there needs to be 13?
+    CRAB_MEATS,
+    BANANAS,
+    ICE_CREAM; // add two more there needs to be 13?
+
+    public static Topping getToppingFromName(String toppingName) {
+        String upperCasedName = toppingName.toUpperCase();
+        switch (upperCasedName){
+            default: return null;
+            case "SQUID": return SQUID;
+            case "CRAB MEAT": return CRAB_MEATS;
+            case "BANANAS": return BANANAS;
+            case "ICE CREAM": return ICE_CREAM;
+            case "HAM": return HAM;
+            case "BLACK OLIVES": return BLACK_OLIVE;
+            case "BEEF": return BEEF;
+            case "SHRIMP": return SHRIMP;
+            case "MUSHROOM": return MUSHROOM;
+            case "SAUSAGE": return SAUSAGE;
+            case "PEPPERONI": return PEPPERONI;
+            case "GREEN PEPPER": return GREEN_PEPPER;
+            case "ONION": return ONION;
+        }
+    }
 }

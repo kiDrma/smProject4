e506933260bb8f4541f36dc23c2a39bfd0ef5907
Kim <kimdonnarumma@gmail.com>
2023-11-27 16:01:26 -0500

    export

--- src/main/java/com/example/smproject4/StoreOrders.java
+++ src/main/java/com/example/smproject4/StoreOrders.java
@@ -11,58 +11,97 @@ import java.nio.file.StandardOpenOption;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class StoreOrders {
     private static int orderNumber = 1;
     private ArrayList<Order> storeOrders;
     private ArrayList<Integer> orderNumbers;
 
-    public StoreOrders(){
+    public StoreOrders() {
         storeOrders = new ArrayList<>();
         orderNumbers = new ArrayList<>();
     }
 
-    public void addOrder(Order order){
+    public void addOrder(Order order) {
         storeOrders.add(order);
         orderNumbers.add(orderNumber);
         orderNumber++;
     }
 
-    public void removeOrder(Order order, int orderNumber){
+    public void removeOrder(Order order, int orderNumber) {
         storeOrders.remove(order);
-        orderNumbers.remove((Integer)orderNumber);
+        orderNumbers.remove((Integer) orderNumber);
     }
 
-    public ArrayList<Integer> getOrderNumbers(){
+    public ArrayList<Integer> getOrderNumbers() {
         return orderNumbers;
     }
 
-    public ArrayList<Order> getOrders(){
+    public ArrayList<Order> getOrders() {
         return storeOrders;
     }
+
     private static int exportedFilesCount = 0;
+
+    /**
     @FXML
-    public void export(){
-        try{
+    public void exportOld() {
+        try {
+
             File file = new File("StoreOrders" + exportedFilesCount + ".txt");
             FileWriter fileWriter = new FileWriter(file);
-            for(int i = 0; i < getOrders().size(); i++){
-                try{
+            for (int i = 0; i < getOrders().size(); i++) {
+                try {
                     fileWriter.write("Order #" + getOrderNumbers().get(i) + "\n");
                     fileWriter.write(getOrders().get(i).displayOrder());
                     String theTotal = new DecimalFormat("0.00").format(getOrders().get(i).getOrderTotal());
                     fileWriter.write("Total: $" + theTotal + "\n\n");
-                }
-                catch(IOException e){
+                } catch (IOException e) {
                     System.out.println("test");
                     return;
                 }
                 exportedFilesCount++;
             }
             fileWriter.close();
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             return;
         }
     }
+     */
+
+    /**
+     * Exports the store orders to a user-selected text file.
+     */
+    @FXML
+    public void export() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export Store Orders");
+        Stage temp = new Stage();
+        File selectedFile = fileChooser.showSaveDialog(temp);
+        if (selectedFile != null) {
+            try {
+                FileWriter fileWriter = new FileWriter(selectedFile);
+                for (int i = 0; i < getOrders().size(); i++) {
+                    try {
+                        fileWriter.write("Order #" + getOrderNumbers().get(i) + "\n");
+                        fileWriter.write(getOrders().get(i).displayOrder());
+                        String theTotal = new DecimalFormat("0.00").format(getOrders().get(i).getOrderTotal());
+                        fileWriter.write("Total: $" + theTotal + "\n\n");
+                    } catch (IOException e) {
+                        System.out.println("test");
+                        return;
+                    }
+                    exportedFilesCount++;
+                }
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error saving file!");
+            }
+        }
+        temp.close();
+    }
 }
+
